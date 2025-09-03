4f14765b16036aabaaa8525a91d149bdda3e10d0
thanhtd <thanhtd@vn.sprite.jp>
2025-09-03 17:51:17 +0700

    update gallery

--- frontend/src/app/app.config.server.ts
+++ frontend/src/app/app.config.server.ts
@@ -2,11 +2,11 @@ import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering, withRoutes } from '@angular/ssr';
 import { appConfig } from './app.config';
 import { serverRoutes } from './app.routes.server';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch  } from '@angular/common/http';
 const serverConfig: ApplicationConfig = {
   providers: [
     provideServerRendering(withRoutes(serverRoutes)),
-    provideHttpClient()
+    provideHttpClient(withFetch())
   ]
 };
 
--- frontend/src/app/components/gallery/gallery.ts
+++ frontend/src/app/components/gallery/gallery.ts
@@ -1,34 +1,33 @@
 import { Component, OnInit, inject, PLATFORM_ID } from '@angular/core';
 import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-gallery',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './gallery.html',
-  styleUrls: ['./gallery.css']
+  styleUrls: ['./gallery.css'],
 })
 export class Gallery implements OnInit {
-  categories: string[] = [];
-  images: { [key: string]: string[] } = {};
-
   private http = inject(HttpClient);
   private platformId = inject(PLATFORM_ID);
 
+  categories$ = new BehaviorSubject<string[]>([]);
+  images$ = new BehaviorSubject<{ [key: string]: string[] }>({});
+
   ngOnInit(): void {
-    // Only load JSON in the browser, not during SSR/prerender
+    // Only load JSON in browser
     if (isPlatformBrowser(this.platformId)) {
       this.http.get<{ [key: string]: string[] }>('assets/gallery.json')
         .subscribe({
           next: (data) => {
-            this.images = data;
-            this.categories = Object.keys(data);
+            this.images$.next(data);
+            this.categories$.next(Object.keys(data));
             console.log('✅ Loaded gallery.json', data);
           },
-          error: (err) => {
-            console.error('❌ Failed to load gallery.json', err);
-          }
+          error: (err) => console.error('❌ Failed to load gallery.json', err)
         });
     }
   }
